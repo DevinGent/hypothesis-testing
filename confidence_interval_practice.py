adf764c4ba1e9266ec7206b7e49d8f30ae4bd85c
Devin Gent <devgent@yahoo.com>
2023-08-12 11:37:23 -0400

    Considered scipy's approach to creating a confidence interval.

--- confidence_interval_practice.py
+++ confidence_interval_practice.py
@@ -4,16 +4,20 @@ import seaborn as sns
 import scipy.stats as st
 import math
 
-# We first establish a population, from a normal distribution, of size 1000 with mean 0 and standard deviation 1.
-pop = np.random.default_rng(1).normal(loc=0, scale=1, size=100000)
-# Note that default_rng(1) makes the random choice reproducible.
+np.random.RandomState(1)
+# Note that np.random.RandomState(1) makes the code's behavior reproducible.
 
+
+# We first establish a population, from a normal distribution, of size 1000 with mean 0 and standard deviation 1.
+pop = np.random.normal(loc=0, scale=1, size=100000)
+
+# Displaying the population.
 sns.kdeplot(pop)
 plt.show()
 
 print('The mean of the population is {}'.format(pop.mean()))
 print("The Z value of the confidence interval 95% is {}".format(st.norm.ppf(.975)))
-# 1.96 is the value we get from looking at a table online.
+# 1.96 is the value we get from looking at a table online, which matches the above.
 
 # We will create a function which gives the confidence interval of a particular random sample from pop.
 
@@ -25,14 +29,18 @@ def get_con_interval(sample,level):
     return (sample.mean()-margin_of_error,sample.mean()+margin_of_error)
 
 # Let's test if this works.  
-sample = np.random.default_rng(1).choice(pop, size=36)
+sample = np.random.choice(pop, size=36)
 print(get_con_interval(sample,.95))
+# In the future we will need the left and right endpoints of the interval.  Let's try to select only one of the endpoints.
 print(get_con_interval(sample,.95)[0])
+
 # We will compare vs computing manually here.
 
 print("The mean and sample deviation of the sample are {} and {}".format(sample.mean(),sample.std()))
 
 print("The margin of error is {}".format(st.norm.ppf(.975)*(sample.std()/6)))
+
+# Comparing Z values.
 print(st.norm.ppf(.975))
 print(st.norm.ppf(.025))
 print("The interval computed manually is",
@@ -58,14 +66,14 @@ print("the population mean was inside the interval {} times and outside {} times
 
 # We will create a function from the above code so we can test at whatever number of tests we like, again and again.
 
-def in_or_out(n, con_level):
+def in_or_out(n, con_level=.95):
     """Takes a number of tests, n, and then generates n samples and confidence intervals (at con_level), collecting the
     number of times the population mean was inside the confidence interval."""
     inside=0
     outside=0
     pop_mean=pop.mean()
     for i in range(n):
-        sample=np.random.default_rng(i).choice(pop, size=36)
+        sample=np.random.choice(pop, size=36)
         interval=get_con_interval(sample,con_level)
         if pop_mean<interval[0] or pop_mean>interval[1]:
             outside=outside+1
@@ -78,8 +86,17 @@ def in_or_out(n, con_level):
     success_percent = round(100*(inside/n),2)
     print("That is, the population mean was inside the interval {}% of the time.".format(success_percent))
 
+# Testing the above function.
 in_or_out(100)
 
-
-for i in np.random.default_rng(1).choice(range(1000), size=10):
+# Testing for 10 different inputs between 1 and 1000.
+for i in np.random.choice(range(1000), size=10):
     in_or_out(i)
+
+# Let's compare our method for getting a confidence interval with the one from scipy.
+
+test_sample=np.random.normal(loc=0, scale=1, size=40)
+print("Our function finds the interval is",get_con_interval(test_sample,.9))
+print("Using scipy the interval is",st.norm.interval(0.9, loc=np.mean(test_sample), scale=np.std(test_sample)/np.sqrt(40)))
+
+# Note that the above only works for large samples (over about 30 elements).  But that was true of our function as well.
